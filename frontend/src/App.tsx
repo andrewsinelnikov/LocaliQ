2c95ae4f66cbe2d1ad1cb55fc7ab83b657350dc2
Andrew Sinelnikov <ansinelnikov@gmail.com>
2025-05-31 14:28:46 +0300

    add App

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,33 @@
+import './style.css';
+import Map from './components/Map';
+import Sidebar from './components/Sidebar';
+import data from './data/producers.json';
+import { useState } from 'react';
+import type { IProducer } from './utils/types';
 
 const App = () => {
-  return (
-    <>
-      <h1>App</h1>
-    </>
-  )
-}
+  const [selectedCategory, setSelectedCategory] = useState('Усі');
 
-export default App
+  const allProducers: IProducer[] = data;
+  const categories = [...new Set(allProducers.map((p) => p.category))];
+
+  const filteredProducers =
+    selectedCategory === 'Усі'
+      ? allProducers
+      : allProducers.filter((p) => p.category === selectedCategory);
+
+  return (
+    <div className="app">
+      <Sidebar
+        categories={categories}
+        selected={selectedCategory}
+        onSelect={setSelectedCategory}
+      />
+      <div className="map-container">
+        <Map producers={filteredProducers} />
+      </div>
+    </div>
+  );
+};
+
+export default App;
