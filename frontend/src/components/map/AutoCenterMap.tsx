7587367235199575a3d21bb758d56d580d4fb69e
Andrew Sinelnikov <ansinelnikov@gmail.com>
2025-06-05 14:35:57 +0300

    add AutoCenterMap

---
+++ frontend/src/components/map/AutoCenterMap.tsx
@@ -0,0 +1,19 @@
+import { useEffect } from 'react';
+import { useMap } from 'react-leaflet';
+
+interface AutoCenterMapProps {
+  lat: number;
+  lng: number;
+}
+
+const AutoCenterMap = ({ lat, lng }: AutoCenterMapProps) => {
+  const map = useMap();
+
+  useEffect(() => {
+    map.setView([lat, lng], 13);
+  }, [lat, lng, map]);
+
+  return null;
+};
+
+export default AutoCenterMap;
