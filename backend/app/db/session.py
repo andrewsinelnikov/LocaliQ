58925cfa43815d3e0ca3894695e1f74e4d215831
Користувач <andrey2005bon@gmail.com>
2025-06-19 11:48:32 +0300

    upd session

--- backend/app/db/session.py
+++ backend/app/db/session.py
@@ -3,11 +3,9 @@ from sqlalchemy.orm import sessionmaker
 from dotenv import load_dotenv
 import os
 
-load_dotenv()
-
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(DATABASE_URL, connect_args={"sslmode": "disable"})
 SessionLocal = sessionmaker(bind=engine)
 
 def get_db():
