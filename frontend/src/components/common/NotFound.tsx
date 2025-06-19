5423dd3d74bb33094a3ae8e67fc46da123eb4557
Andrew Sinelnikov <ansinelnikov@gmail.com>
2025-06-19 20:19:05 +0300

    add lang

--- frontend/src/components/common/NotFound.tsx
+++ frontend/src/components/common/NotFound.tsx
@@ -8,15 +8,15 @@ const NotFoundPage = () => {
   return (
     <div className="notfound-page">
       <div className="notfound-wrapper">
-        <h2>{t('notFound.title', 'Ой! Нічого не знайдено')}</h2>
-        <p>{t('notFound.message', 'Ми не змогли знайти цю сторінку')}</p>
+        <h2>{t('notFound.title')}</h2>
+        <p>{t('notFound.message')}</p>
 
         <div className="notfound-buttons">
           <Link to="/" className="notfound-link">
-            {t('notFound.goHome', 'На головну вітрину')}
+            {t('notFound.goHome')}
           </Link>
           <Link to="/map" className="notfound-link">
-            {t('notFound.showMap', 'Показати мапу виробників')}
+            {t('notFound.showMap')}
           </Link>
         </div>
 
