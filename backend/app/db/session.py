d4fa011f6d4e3c9b6099c2158f96cce1d9d37e97
Користувач <andrey2005bon@gmail.com>
2025-06-19 10:49:07 +0300

    restore data in session

--- backend/app/db/session.py
+++ backend/app/db/session.py
@@ -7,8 +7,8 @@ load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(DATABASE_URL, connect_args={"sslmode": "disable"})
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(bind=engine)
 
 def get_db():
     db = SessionLocal()
