8a10b271b375f078c6b74e58ab15ad957bad9433
Користувач <andrey2005bon@gmail.com>
2025-06-19 12:01:19 +0300

    restore files

--- backend/app/db/session.py
+++ backend/app/db/session.py
@@ -3,9 +3,11 @@ from sqlalchemy.orm import sessionmaker
 from dotenv import load_dotenv
 import os
 
+load_dotenv()
+
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(DATABASE_URL, connect_args={"sslmode": "disable"})
+engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine)
 
 def get_db():
