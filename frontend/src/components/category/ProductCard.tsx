4966d1c8ce2ab17a78333d0ae8c1d4656bbf8461
Andrew Sinelnikov <ansinelnikov@gmail.com>
2025-06-20 13:41:26 +0300

    ProductCard component

--- frontend/src/components/category/ProductCard.tsx
+++ frontend/src/components/category/ProductCard.tsx
@@ -1,9 +1,19 @@
-import React from 'react'
-
-const ProductCard = () => {
-  return (
-    <div>ProductCard</div>
-  )
+interface IProductCardProps {
+  name: string;
+  imageUrl: string;
+  price: string;
 }
 
-export default ProductCard
+const ProductCard = ({ name, imageUrl, price }: IProductCardProps) => {
+  return (
+    <div className="category-stand">
+      <img src={imageUrl} alt={name} />
+      <div className="category-stand-name">
+        <div>{name}</div>
+        <div style={{ color: '#0077cc', fontWeight: '500', marginTop: '4px' }}>{price}</div>
+      </div>
+    </div>
+  );
+};
+
+export default ProductCard;
