03749eabbb029a19f6174bb6c6b7644b7d1dc59b
Andrew Sinelnikov <ansinelnikov@gmail.com>
2025-06-20 13:39:55 +0300

    add CategorySection component

--- frontend/src/components/category/CategorySection.tsx
+++ frontend/src/components/category/CategorySection.tsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import ProductCard from './ProductCard';
 
-const CategorySection = () => {
-  return (
-    <div>CategorySection</div>
-  )
+interface ICategorySectionProps {
+  category: string;
+  products: {
+    id: number;
+    name: string;
+    imageUrl: string;
+    price: string;
+  }[];
 }
 
-export default CategorySection
+const CategorySection = ({ category, products }: ICategorySectionProps) => {
+  return (
+    <div className="category-section">
+      <h2 className="category-title">{category}</h2>
+      <div className="category-stands-grid">
+        {products.map((product) => (
+          <ProductCard
+            key={product.id}
+            name={product.name}
+            imageUrl={product.imageUrl}
+            price={product.price}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default CategorySection;
