d9c2ed3a443c93137cc50571c289e92182214813
Andrew Sinelnikov <ansinelnikov@gmail.com>
2026-03-06 22:07:26 +0200

    add image

--- frontend/src/components/common/categoriesConfig.ts
+++ frontend/src/components/common/categoriesConfig.ts
@@ -1319,17 +1319,17 @@ export const categories: ICategory[] = [
         imageUrl: '/images/subcategories/wellness/mental-wellness-coaching.jpg',
         description: 'categories.wellness-care.subcategories.mental-wellness-coaching.description',
         items: [
-          { name: 'Сесія з психологом', imageUrl: '/images/products/psych-consultation.jpg', description: 'Професійна допомога для покращення настрою та  подолання життєвих труднощів' },
-          { name: 'Стрес-менеджмент', imageUrl: '/images/products/stress-coaching.jpg', description: 'Підтримка у знятті стресу та тривоги' },
-          { name: 'Сімейна терапія', imageUrl: '/images/products/family-therapy.jpg', description: 'Покращення взаєморозуміння та комунікації в родині' },
-          { name: 'Для дітей і підлітків', imageUrl: '/images/products/child-therapy.jpg', description: 'Делікатна допомога дітям у вирішенні емоційних питань' },
-          { name: 'Групова терапія', imageUrl: '/images/products/group-therapy.jpg', description: 'Спільна робота над емоціями в підтримуючому колі' },
-          { name: 'Емоційний інтелект', imageUrl: '/images/products/emotional-intelligence.jpg', description: 'Розвиток навичок розпізнавання та керування емоціями' },
-          { name: 'Коучинг-сесії', imageUrl: '/images/products/coaching-session.jpg', description: 'Допомога у досягненні цілей і розкритті потенціалу' },
-          { name: 'Коучинг лідерства', imageUrl: '/images/products/leadership-coaching.jpg', description: 'Розвиток управлінських навичок та впевненості' },
-          { name: 'Кар’єрний коучинг', imageUrl: '/images/products/career-coaching.jpg', description: 'Підтримка у виборі професійного шляху та розвитку кар’єри' },
-          { name: 'Коучинг для бізнесу', imageUrl: '/images/products/business-coaching.jpg', description: 'Підтримка підприємців у розвитку проектів та команд' },          
-          { name: 'Кризова допомога', imageUrl: '/images/products/crisis-support.jpg', description: 'Невідкладна підтримка у важких емоційних станах' }
+          { name: 'Сесія з психологом', imageUrl: '/images/products/wellness/mental-wellness-coaching/psych-consultation.jpg', description: 'Професійна допомога для покращення настрою та  подолання життєвих труднощів' },
+          { name: 'Стрес-менеджмент', imageUrl: '/images/products/wellness/mental-wellness-coaching/stress-coaching.jpg', description: 'Підтримка у знятті стресу та тривоги' },
+          { name: 'Сімейна терапія', imageUrl: '/images/products/wellness/mental-wellness-coaching/family-therapy.jpg', description: 'Покращення взаєморозуміння та комунікації в родині' },
+          { name: 'Для дітей і підлітків', imageUrl: '/images/products/wellness/mental-wellness-coaching/child-therapy.jpg', description: 'Делікатна допомога дітям у вирішенні емоційних питань' },
+          { name: 'Групова терапія', imageUrl: '/images/products/wellness/mental-wellness-coaching/group-therapy.jpg', description: 'Спільна робота над емоціями в підтримуючому колі' },
+          { name: 'Емоційний інтелект', imageUrl: '/images/products/wellness/mental-wellness-coaching/emotional-intelligence.jpg', description: 'Розвиток навичок розпізнавання та керування емоціями' },
+          { name: 'Коучинг-сесії', imageUrl: '/images/products/wellness/mental-wellness-coaching/coaching-session.jpg', description: 'Допомога у досягненні цілей і розкритті потенціалу' },
+          { name: 'Коучинг лідерства', imageUrl: '/images/products/wellness/mental-wellness-coaching/leadership-coaching.jpg', description: 'Розвиток управлінських навичок та впевненості' },
+          { name: 'Кар’єрний коучинг', imageUrl: '/images/products/wellness/mental-wellness-coaching/career-coaching.jpg', description: 'Підтримка у виборі професійного шляху та розвитку кар’єри' },
+          { name: 'Коучинг для бізнесу', imageUrl: '/images/products/wellness/mental-wellness-coaching/business-coaching.jpg', description: 'Підтримка підприємців у розвитку проектів та команд' },          
+          { name: 'Кризова допомога', imageUrl: '/images/products/wellness/mental-wellness-coaching/crisis-support.jpg', description: 'Невідкладна підтримка у важких емоційних станах' }
         ],
       },
       {
